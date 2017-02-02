2ee7d961a5de9997b3d1a45d5022e3dcb390ddaa
Matt-Stevens <cantankerous27>
2017-02-02 11:00:01 +0000

    longitude and latitude input implemented

--- app/src/main/java/com/example/a2stevm21/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2stevm21/mapping/HelloMap.java
@@ -3,17 +3,23 @@ package com.example.a2stevm21.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class HelloMap extends Activity {
+public class HelloMap extends Activity implements View.OnClickListener {
     MapView mv;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
 
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         mv =(MapView)findViewById(R.id.map1);
@@ -21,4 +27,13 @@ public class HelloMap extends Activity {
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1, 22.5));
     }
-        }
+
+    @Override
+    public void onClick(View view) {
+        EditText latitudeEditText = (EditText) findViewById(R.id.latitudeInput);
+        double latitdue = Double.parseDouble(latitudeEditText.getText().toString());
+
+        EditText longitudeEditText = (EditText) findViewById(R.id.longitudeInput);
+        double longitude = Double.parseDouble(longitudeEditText.getText().toString());
+    }
+}
